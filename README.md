9f569587d41d0d2e474c3d4c07d4f7401a0896dd
Junior Barros <junior@boletobancario.com>
2017-02-21 14:01:52 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,123 @@
+# BoletoBancario.com - Boleto Fácil
+## SDK para integração com o Boleto Fácil
+
+Para verificar a documentação completa e os exemplos de resposta, acesse o <a href="https://www.boletobancario.com/boletofacil/integration/integration.html" target="_blank">Manual de Integração</a>.
+
+
+
+## Como usar: ##
+
+Baixe e coloque o arquivo **boletofacil.php** no mesmo diretorio do arquivo PHP que irá implementar.
+Após isso, importe o arquivo em seu projeto e inicialize usando seu <a href="https://www.boletobancario.com/boletofacil/integration/integration.html#token" target="_blank">Token de integração</a>:
+
+```php
+require 'boletofacil.php';
+use BoletoFacil\BoletoFacil;
+
+$boletoFacil = new BoletoFacil("SEU_TOKEN");
+```
+
+
+
+## Gerar cobrança mínima: ##
+
+Chame o metodo **createCharge** para gerar uma cobrança com os dados mínimos:
+
+```php
+$boletoFacil->createCharge("Junior Barros", "CPF_DO_CLIENTE", "Pedido 48192", "147.36", "07/11/2017");
+```
+
+
+
+## Gerar cobrança: ##
+
+Chame o metodo **createCharge** para gerar uma cobrança com os dados mínimos, e depois adicione mais atributos a cobrança, como por exemplo parcelas e o email do cliente (para envio automatico do email):
+
+```php
+$boletoFacil->createCharge("Junior Barros", "CPF_DO_CLIENTE", "Pedido 48192", "1047.36", "07/11/2017");
+$boletoFacil->installments = 10;
+$boletoFacil->payerEmail = "junior@boletobancario.com";
+```
+
+
+
+## Emitir cobrança: ##
+
+Após gerar uma cobrança, você deverá chamar o método **issueCharge** para emitir a cobrança:
+
+```php
+$boletoFacil->createCharge("Junior Barros", "CPF_DO_CLIENTE", "Pedido 48192", "147.36", "07/11/2017");
+$boletoFacil->payerEmail = "junior@boletobancario.com";
+
+$boletoFacil->issueCharge();
+```
+
+
+
+## Notificação de pagamentos ##
+
+Você pode configurar o Boleto Fácil para enviar notificações para seu sistema sempre que houver um evento de pagamento.
+Para isso, faça login no <a href="https://www.boletobancario.com/boletofacil/user/login.html" target="_blank">Boleto Fácil</a>, e acesse a página de integração <a href="https://www.boletobancario.com/boletofacil/integration/integration.html#notificacao" target="_blank">Notificação de pagamentos</a>.
+
+
+
+
+## Consulta de pagamentos: ##
+
+Após receber uma notificação de pagamento, seu sistema pode utilizar o paymentToken recebido para consultar detalhes do pagamento:
+
+```php
+require 'boletofacil.php';
+use BoletoFacil\BoletoFacil;
+
+$boletoFacil = new BoletoFacil("SEU_TOKEN");
+$boletoFacil->fetchPaymentDetails("PAYMENT_TOKEN");
+```
+
+
+
+
+## Consulta de saldo: ##
+
+Você pode verificar seu saldo através do método:
+
+```php
+require 'boletofacil.php';
+use BoletoFacil\BoletoFacil;
+
+$boletoFacil = new BoletoFacil("SEU_TOKEN");
+$boletoFacil->fetchBalance();
+```
+
+
+
+
+## Solicitação de transferência: ##
+
+Você pode solicitar a transferência dos valores em saldo usando o seguinte método:
+
+```php
+require 'boletofacil.php';
+use BoletoFacil\BoletoFacil;
+
+$boletoFacil = new BoletoFacil("SEU_TOKEN");
+$boletoFacil->requestTransfer("232.22");
+```
+
+
+
+
+## Cancelamento de cobrança: ##
+
+Para arquivar e cancelar os lembretes de uma cobrança, use o método:
+
+```php
+require 'boletofacil.php';
+use BoletoFacil\BoletoFacil;
+
+$boletoFacil = new BoletoFacil("SEU_TOKEN");
+$boletoFacil->cancelCharge("CODIGO_DA_COBRANCA");
+```
+
+
+Para verificar a documentação completa e os exemplos de resposta, acessar <a href="https://www.boletobancario.com/boletofacil/integration/integration.html" target="_blank">Manual de Integração</a>.
---
+++ boletofacil.php
@@ -0,0 +1,159 @@
+<?php
+
+/*
+ *
+ * BoletoFacil é uma solução para emissão de cobranças da BoletoBancario.com
+ * Para usar, é necessário ter um cadastro no boleto fácil e gerar um token de integração.
+ * Acesse e confira: https://www.boletobancario.com/boleto-facil
+ * Documentação: https://www.boletobancario.com/boletofacil/integration/integration.html
+ * Criado por: Junior Barros
+ *
+ */
+
+namespace BoletoFacil;
+
+class BoletoFacil {
+
+    public $description;
+    public $reference;
+    public $amount;
+    public $dueDate;
+    public $installments;
+    public $maxOverdueDays;
+    public $fine;
+    public $interest;
+    public $discountAmount;
+    public $discountDays;
+
+    public $payerName;
+    public $payerCpfCnpj;
+    public $payerEmail;
+    public $payerSecondaryEmail;
+    public $ayerPhone;
+    public $payerBirthDate;
+
+    public $billingAddressStreet;
+    public $billingAddressNumber;
+    public $billingAddressComplement;
+    public $billingAddressCity;
+    public $billingAddressState;
+    public $billingAddressPostcode;
+
+    public $notifyPayer;
+    public $notificationUrl;
+    
+    private $token;
+    private $sandbox;
+
+    const PROD_URL = "https://www.boletobancario.com/boletofacil/integration/api/v1/";
+    const SANDBOX_URL = "https://sandbox.boletobancario.com/boletofacil/integration/api/v1/";
+    const RESPONSE_TYPE = "JSON";
+
+    function __construct($token, $sandbox = false) {
+        $this->token = $token;
+        $this->sandbox = $sandbox;
+    }
+
+    public function createCharge($payerName, $payerCpfCnpj, $description, $amount, $dueDate) {
+        $this->payerName = $payerName;
+        $this->payerCpfCnpj = $payerCpfCnpj;
+        $this->description = $description;
+        $this->amount = $amount;
+        $this->dueDate = $dueDate;
+        return $this;
+    }
+
+    public function issueCharge() {
+        $requestData = array(
+            'token'                     =>  $this->token,
+            'description'               =>  $this->description,
+            'reference'                 =>  $this->reference,
+            'amount'                    =>  $this->amount,
+            'dueDate'                   =>  $this->dueDate,
+            'installments'              =>  $this->installments,
+            'maxOverdueDays'            =>  $this->maxOverdueDays,
+            'fine'                      =>  $this->fine,
+            'interest'                  =>  $this->interest,
+            'discountAmount'            =>  $this->discountAmount,
+            'discountDays'              =>  $this->discountDays,
+            'payerName'                 =>  $this->payerName,
+            'payerCpfCnpj'              =>  $this->payerCpfCnpj,
+            'payerEmail'                =>  $this->payerEmail,
+            'payerSecondaryEmail'       =>  $this->payerSecondaryEmail,
+            'payerPhone'                =>  $this->payerPhone,
+            'payerBirthDate'            =>  $this->payerBirthDate,
+            'billingAddressStreet'      =>  $this->billingAddressStreet,
+            'billingAddressNumber'      =>  $this->billingAddressNumber,
+            'billingAddressComplement'  =>  $this->billingAddressComplement,
+            'billingAddressCity'        =>  $this->billingAddressCity,
+            'billingAddressState'       =>  $this->billingAddressState,
+            'billingAddressPostcode'    =>  $this->billingAddressPostcode,
+            'notifyPayer'               =>  $this->notifyPayer,
+            'notificationUrl'           =>  $this->notificationUrl,
+            'responseType'              =>  BoletoFacil::RESPONSE_TYPE
+        );
+
+        return $this->request("issue-charge", $requestData);
+    }
+
+    public function fetchPaymentDetails($paymentToken) {
+        $requestData = array(
+            'paymentToken'   => $paymentToken,
+            'responseType'   => BoletoFacil::RESPONSE_TYPE
+        );
+
+        return $this->request("fetch-payment-details", $requestData);
+    }
+
+
+    public function fetchBalance() {
+        $requestData = array(
+            'token'         => $this->token,
+            'responseType'  => BoletoFacil::RESPONSE_TYPE
+        );
+
+        return $this->request("fetch-balance", $requestData);
+    }
+
+
+    public function requestTransfer($amount) {
+        $requestData = array(
+            'token'         => $this->token,
+            'amount'        => $amount,
+            'responseType'  => BoletoFacil::RESPONSE_TYPE
+        );
+
+        return $this->request("request-transfer", $requestData);
+    }
+
+
+    public function cancelCharge($code) {
+        $requestData = array(
+            'token'         => $this->token,
+            'code'          => $code,
+            'responseType'  => BoletoFacil::RESPONSE_TYPE
+        );
+
+        return $this->request("cancel-charge", $requestData);
+    }
+
+
+    private function request($urlSufix, $data) {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => ($this->sandbox ? BoletoFacil::SANDBOX_URL : BoletoFacil::PROD_URL).$urlSufix,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "UTF-8",
+            CURLOPT_MAXREDIRS => 2,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => http_build_query($data) . "\n",
+            CURLOPT_HTTPHEADER => $data
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return $response;
+    }
+
+}
